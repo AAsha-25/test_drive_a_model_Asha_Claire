efdf3ba66d980d992fa0ba9acc0a8e3f16d46f09
Asha <Ashadam2162@gmail.com>
2025-10-22 17:01:28 +0100

    all tests pass in book_store class

--- lib/book_store.py
+++ lib/book_store.py
@@ -3,3 +3,9 @@ class Book():
         self.id = id
         self.title = title
         self.author_name = author_name
+
+    def __repr__(self):
+        return f"Book({self.id}, {self.title}, {self.author_name})"
+    
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
--- tests/test_book_store.py
+++ tests/test_book_store.py
@@ -10,3 +10,31 @@ def test_book_constructs():
     assert book.title == "Test Book"
     assert book.author_name == "Test author_name"
 
+"""
+We can format books to strings nicely
+"""
+def test_books_format_nicely():
+    book = Book(3, 'Emma', 'Jane Austen')
+    assert str(book) == "Book(3, Emma, Jane Austen)"
+    # Try commenting out the `__repr__` method in lib/book.py
+    # And see what happens when you run this test again.
+
+"""
+We can compare two identical books
+And have them be equal
+"""
+def test_books_are_equal():
+    book1 = Book(4, 'Dracula', 'Bram Stoker')
+    book2 = Book(4, 'Dracula', 'Bram Stoker')
+    assert book1 == book2
+
+"""
+Constructs with 
+id, title, author_name, 
+"""
+def test_construsts_with_fields():
+    book = Book(4, 'Dracula', 'Bram Stoker')
+    assert book.id == 4
+    assert book.title == 'Dracula'
+    assert book.author_name == 'Bram Stoker'
+    
